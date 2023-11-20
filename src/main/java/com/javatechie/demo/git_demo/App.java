5745e0ae9f90560df3efa832a3805a1feb0d9a6b
khantariqu <mohdtariquemhk@gmail.com>
2023-11-21 00:23:27 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/demo/git_demo/App.java
@@ -0,0 +1,16 @@
+package com.javatechie.demo.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void m1() {
+		System.out.println("Inside mi() method");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
